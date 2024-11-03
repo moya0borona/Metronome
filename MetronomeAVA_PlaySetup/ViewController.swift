cb684ec37da268e25874a0fd5f80f002916f8f1c
AW <idmese@gmail.com>
2024-11-03 15:16:17 +0300

    Initial Commit

---
+++ MetronomeAVA_PlaySetup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetronomeAVA_PlaySetup
+//
+//  Created by Андрей Андриянов on 03.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

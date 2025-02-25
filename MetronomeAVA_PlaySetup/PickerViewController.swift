ea1b59794ac6cfa78642629ee1ea091bd667724f
AW <idmese@gmail.com>
2025-02-25 13:31:35 +0300

    changed delegate

--- MetronomeAVA_PlaySetup/PickerViewController.swift
+++ MetronomeAVA_PlaySetup/PickerViewController.swift
@@ -13,7 +13,6 @@ protocol PickerDelegate: AnyObject {
 class PickerViewController: UIViewController {
     
     weak var delegate: PickerDelegate?
-    
     let pickerView = UIPickerView()
     var pickerViewValue = ["1", "2", "3", "4", "5", "6", "7", "8"]
     
